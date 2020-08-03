2dc2aef76b04dc5abb1d81109e2f11f0f6857bea
Neeraj <neerbalodi8@gmail.com>
2020-08-03 20:04:03 +0530

    Added Header Component & Shop Items

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-  
+
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from "./Pages/HomePage/homepage";
+import ShopPage from "./Pages/shop/shop";
+import Header from "./Components/header/header";
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ src/Pages/shop/shop.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ShopData from "./shopData";
+import CollectionPreview from "../../Components/Collection-Preview/CollectionPreview";
+
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collections: ShopData,
+    };
+  }
+  render() {
+    const { collections } = this.state;
+    return (<div className="shop-page">
+      
+      {
+
+         collections.map(({id , ...otherCollectionProps}) => (
+
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))
+
+      }
+      
+      
+      </div>);
+  }
+}
+
+export default ShopPage;

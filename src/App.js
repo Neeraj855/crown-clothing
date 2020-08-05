894ccbf462987686b3c0f3d30a543673c2d39516
Neeraj <neerbalodi8@gmail.com>
2020-08-05 09:34:37 +0530

    Added SignIn page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import HomePage from "./Pages/HomePage/homepage";
 import ShopPage from "./Pages/shop/shop";
 import Header from "./Components/header/header";
+import SignInAndSignUpPage from "./Pages/sign-in&sign-up/sign-in&sign-up";
 
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
+
       </Switch>
     </div>
   );
